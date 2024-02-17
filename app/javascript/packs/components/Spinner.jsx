dff7bb52efa712c11cba32c747358e5f67840b17
Jagdish Kumar <jk.thakur1285@gmail.com>
2024-02-17 17:30:40 +0530

    changed loader

--- app/javascript/packs/components/Spinner.jsx
+++ app/javascript/packs/components/Spinner.jsx
@@ -4,8 +4,8 @@ const Spinner = () => {
   return (
     <div className="d-flex align-items-center justify-content-center py-5">
       <div className="spinner-border" role="status">
-        <span className="sr-only">Loading...</span>
       </div>
+      <span className="sr-only">Loading...</span>
     </div>
   );
 };
